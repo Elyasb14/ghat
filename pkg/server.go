411f36acdf82f9664f07906ed3d84502d0785ad9
Elyas Bianchi <elyasb99@gmail.com>
2024-03-08 11:37:53 -0800

    go fmt

--- pkg/server.go
+++ pkg/server.go
@@ -1,10 +1,10 @@
 package server
 
 import (
-  "fmt"
-  "net"
-  "log"
-  "os"
+	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 var logFile *os.File
@@ -12,49 +12,48 @@ var err error
 
 // this gets called implicitly when the server package gets called
 func init() {
-  logPath := "ghat.log"
-  
-  logFile, err = os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-  if err != nil {
-    fmt.Println("can't open log file:", err)
-    return
-  }
-  log.SetOutput(logFile)
+	logPath := "ghat.log"
+
+	logFile, err = os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("can't open log file:", err)
+		return
+	}
+	log.SetOutput(logFile)
 }
 
 func StartTCPServer() {
-  
 
-  ln, err := net.Listen("tcp", ":9999")
-  log.Println("server init")
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  for {
-    conn, err := ln.Accept()
-    if err != nil {
-      fmt.Println(err)
-      continue
-    }
-    log.Println("connection accepted from", conn.RemoteAddr())
-    
-    go handleTCPConnection(conn)
+	ln, err := net.Listen("tcp", ":9999")
+	log.Println("server init")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		log.Println("connection accepted from", conn.RemoteAddr())
 
-  }
+		go handleTCPConnection(conn)
+
+	}
 }
 
 func handleTCPConnection(conn net.Conn) {
-  defer conn.Close()
-  
-  for {
-    buf := make([]byte, 2048)
-    n, err := conn.Read(buf)
-    if err != nil {
-      fmt.Println(err)
-      return
-    }
-    str := string(buf[:n])
-    fmt.Println(str) 
-  }
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 2048)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		str := string(buf[:n])
+		fmt.Println(str)
+	}
 }
