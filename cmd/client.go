feef860302cb57f3963929279777575a32fe391c
Elyas Bianchi <elyasb99@gmail.com>
2024-03-08 10:54:05 -0800

    packet stuff

--- cmd/client.go
+++ cmd/client.go
@@ -8,25 +8,32 @@ import (
 	"os"
 )
 
+type Packet struct {
+	sourceAddr string
+	destAddr   string
+	data       string
+}
+
 func main() {
 	conn, err := net.Dial("tcp", "localhost:9999")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-  defer conn.Close()
-  for {
-    reader := bufio.NewReader((os.Stdin))
-    fmt.Println("enter your chat: ")
-    char, _, err := reader.ReadLine()
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-    _, err = conn.Write([]byte(char))
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-  }
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader((os.Stdin))
+		fmt.Println("enter your chat: ")
+		char, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		_, err = conn.Write([]byte(char))
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
 }
