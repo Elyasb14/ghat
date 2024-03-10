7a375637bd1e83d1c47475ea31eab6dffb99988f
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-03-10 14:14:48 -0700

    got the clients communicating over channels, need to sort of sync them

--- pkg/server.go
+++ pkg/server.go
@@ -31,6 +31,13 @@ func StartTCPServer() {
 		log.Println(err)
 		return
 	}
+  
+  // make a channel I can send packets through, make a hashmap of clients
+  // pass in the channel and clients to the broadcast go routine
+  messageChan := make(chan Packet)
+  clients := make(map[net.Conn]struct{})
+  go broadcastMessages(messageChan, clients)
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -38,14 +45,21 @@ func StartTCPServer() {
 			continue
 		}
 		log.Println("connection accepted from:", conn.RemoteAddr())
+     
+    // add the new connection to the client hashmap, this will get passed into the 
+    // broadcastMessage goroutine
+    clients[conn] = struct{}{}
 
-		go handleTCPConnection(conn)
+		go handleTCPConnection(conn, messageChan)
 
 	}
 }
 
-func handleTCPConnection(conn net.Conn) {
-	defer conn.Close()
+func handleTCPConnection(conn net.Conn, messageChan chan<- Packet) {
+	defer func() {
+    conn.Close()
+    // delete(clients, conn)
+  }()
 	for {
 		buf := make([]byte, 2048)
 		n, err := conn.Read(buf)
@@ -59,7 +73,20 @@ func handleTCPConnection(conn net.Conn) {
 		}
 
     log.Printf("message: %s from client @ ip addr: %s", buf[:n], conn.RemoteAddr())
-    conn.Write(packet.sender) 
-    conn.Write(packet.message)
+    //conn.Write(packet.sender) 
+    //conn.Write(packet.message)
+    //send the packet to the message channel
+    messageChan <- packet
 	}
 }
+
+func broadcastMessages(messageChan <- chan Packet, clients map[net.Conn]struct{}) {
+  for {
+    // recieve packet from messageChan, save it into packet, nice syntax!
+    packet := <- messageChan
+    for conn := range clients {
+      conn.Write(append(packet.sender, packet.message...))
+    }
+  }
+}
+
