c5fabd85a1857a9d480764a985ce9b10aeebbcad
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-03-10 14:27:33 -0700

    we have newlines now

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,22 +8,14 @@ import (
 	"os"
 )
 
-type Packet struct {
-	sourceAddr string
-	destAddr   string
-	data       string
-}
-
-
-
 func main() {
-  logPath := "ghat.log"
-  logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-  if err != nil {
-    log.Fatal("can't open log file:", err)
-    return
-  }
-  log.SetOutput(logFile)
+	logPath := "ghat.log"
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal("can't open log file:", err)
+		return
+	}
+	log.SetOutput(logFile)
 	conn, err := net.Dial("tcp", "localhost:9999")
 	if err != nil {
 		log.Fatal(err)
@@ -44,13 +36,13 @@ func main() {
 			log.Fatal(err)
 			return
 		}
-    
-    buf := make([]byte, 2048)
-    n, err := conn.Read(buf)
-    if err != nil {
-      log.Fatal(err)
-      return
-    }
-    fmt.Println(string(buf[:n]))
+
+		buf := make([]byte, 2048)
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
 	}
 }
--- pkg/server.go
+++ pkg/server.go
@@ -19,8 +19,8 @@ func init() {
 }
 
 type Packet struct {
-  sender []byte
-  message []byte
+	sender  []byte
+	message []byte
 }
 
 func StartTCPServer() {
@@ -31,24 +31,24 @@ func StartTCPServer() {
 		log.Println(err)
 		return
 	}
-  
-  // make a channel I can send packets through, make a hashmap of clients
-  // pass in the channel and clients to the broadcast go routine
-  messageChan := make(chan Packet)
-  clients := make(map[net.Conn]struct{})
-  go broadcastMessages(messageChan, clients)
+
+	// make a channel I can send packets through, make a hashmap of clients
+	// pass in the channel and clients to the broadcast go routine
+	messageChan := make(chan Packet)
+	clients := make(map[net.Conn]struct{})
+	go broadcastMessages(messageChan, clients)
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-		 log.Println(err)
+			log.Println(err)
 			continue
 		}
 		log.Println("connection accepted from:", conn.RemoteAddr())
-     
-    // add the new connection to the client hashmap, this will get passed into the 
-    // broadcastMessage goroutine
-    clients[conn] = struct{}{}
+
+		// add the new connection to the client hashmap, this will get passed into the
+		// broadcastMessage goroutine
+		clients[conn] = struct{}{}
 
 		go handleTCPConnection(conn, messageChan)
 
@@ -57,36 +57,36 @@ func StartTCPServer() {
 
 func handleTCPConnection(conn net.Conn, messageChan chan<- Packet) {
 	defer func() {
-    conn.Close()
-    // delete(clients, conn)
-  }()
+		conn.Close()
+		// delete(clients, conn)
+	}()
 	for {
 		buf := make([]byte, 2048)
 		n, err := conn.Read(buf)
-    packet := Packet{
-      sender:  []byte("from " + conn.RemoteAddr().String() + ": "),
-      message: buf[:n],
-    }
+		packet := Packet{
+			sender:  []byte("from " + conn.RemoteAddr().String() + ": "),
+			message: buf[:n],
+		}
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
-    log.Printf("message: %s from client @ ip addr: %s", buf[:n], conn.RemoteAddr())
-    //conn.Write(packet.sender) 
-    //conn.Write(packet.message)
-    //send the packet to the message channel
-    messageChan <- packet
+		log.Printf("message: %s from client @ ip addr: %s", buf[:n], conn.RemoteAddr())
+		//conn.Write(packet.sender)
+		//conn.Write(packet.message)
+		//send the packet to the message channel
+		messageChan <- packet
 	}
 }
 
-func broadcastMessages(messageChan <- chan Packet, clients map[net.Conn]struct{}) {
-  for {
-    // recieve packet from messageChan, save it into packet, nice syntax!
-    packet := <- messageChan
-    for conn := range clients {
-      conn.Write(append(packet.sender, packet.message...))
-    }
-  }
+func broadcastMessages(messageChan <-chan Packet, clients map[net.Conn]struct{}) {
+	for {
+		// recieve packet from messageChan, save it into packet, nice syntax!
+		packet := <-messageChan
+	  for conn := range clients {
+      msgNl := append(packet.message, []byte("\n")...)
+			conn.Write(append(packet.sender, msgNl...))
+		}
+	}
 }
-
