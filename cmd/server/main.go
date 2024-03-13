797aee10e94a3f21def905901e712bd2e23dc449
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-03-13 16:42:51 -0700

    need to think harder about how I want to handle the db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	//"ebianchi/ghat/pkg"
-  db "ebianchi/ghat/pkg/database"
-  "fmt"
+	// "ebianchi/ghat/pkg/server"
+	db "ebianchi/ghat/pkg/database"
+	"fmt"
 )
 
 func main() {
 	//server.StartTCPServer()
-  err := db.InitDB("file:///tmp/ghat.db")
-  if err != nil {
-    fmt.Println(err)
-  }
+	err := db.InitDB("file:///tmp/ghat.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println(db.Db)
+	db.SaveChat()
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.ShowTable()
 }
--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,9 +1,13 @@
 package database
 
 import (
-  "database/sql"
+	"database/sql"
+	"time"
+	"log"
+	"fmt"
 
-  _ "github.com/tursodatabase/go-libsql"
+	_ "github.com/tursodatabase/go-libsql"
+	//server "ebianchi/ghat/pkg/server"
 )
 
 var Db *sql.DB
@@ -12,21 +16,48 @@ var Db *sql.DB
 // use the env var DB_URL
 func InitDB(url string) error {
 
-  db, err := sql.Open("libsql", url)  
+	db, err := sql.Open("libsql", url)
 
-  if err != nil {
-    return err
-  }
-  
-  db.Exec(`CREATE TABLE IF NOT EXISTS chats (
+	if err != nil {
+		return err
+	}
+
+	db.Exec(`CREATE TABLE IF NOT EXISTS chats (
            id SERIAL PRIMARY KEY,
            time TIMESTAMP NOT NULL,
            message TEXT NOT NULL,
            ip_address TEXT NOT NULL
   )`)
 
-  Db = db
-  return nil
+	Db = db
+	return nil
 }
 
+// TODO: change this to accept *server.Packet
+func SaveChat() error {
+	stmt, err := Db.Prepare("INSERT INTO chats (time, message, ip_address) VALUES ($1, $2, $3)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
+	stmt.Exec(time.Now(), "Hello, world!", "192.168.1.100")
+	return nil
+}
+
+func ShowTable() {
+	rows, err := Db.Query("SELECT time, message, ip_address FROM chats")
+	if err != nil {
+			log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+			var time time.Time
+			var message, ipAddress string
+			if err := rows.Scan(&time, &message, &ipAddress); err != nil {
+					log.Fatal(err)
+			}
+			fmt.Printf("Time: %v, Message: %s, IP Address: %s\n", time, message, ipAddress)
+	}
+}
