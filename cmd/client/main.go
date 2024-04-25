a1cf2dfabcddc890c1e9cd00881f3941ba255396
Elyas Bianchi <elyasb99@gmail.com>
2024-04-25 15:18:24 -0700

    client is working, but after first message we get a server error that says use of closed network connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,6 +6,7 @@ import (
     "fmt"
     "net"
     "os"
+    "github.com/Elyasb14/ghat/pkg/client"
 )
 
 func main() {
@@ -22,49 +23,17 @@ func main() {
 
     // Create a new reader for reading from the standard input
     reader := bufio.NewReader(os.Stdin)
-
-    // Create a new channel for receiving messages from the server
     messages := make(chan string)
 
-    // Start a new goroutine to read messages from the server
-    go func() {
-        buf := make([]byte, 1024)
-        for {
-            n, err := conn.Read(buf)
-            if err != nil {
-                fmt.Println("Error reading from server:", err)
-                return
-            }
-            messages <- string(buf[:n])
-        }
-    }()
-
-    // Start a new goroutine to read user input
-    go func() {
-        for {
-            fmt.Print("> ")
-            input, err := reader.ReadString('\n')
-            if err != nil {
-                fmt.Println("Error reading input:", err)
-                continue
-            }
-
-            // Remove the newline character from the input
-            input = input[:len(input)-1]
-
-            // Send the input to the server
-            _, err = conn.Write([]byte(input))
-            if err != nil {
-                fmt.Println("Error sending message:", err)
-                continue
-            }
-        }
-    }()
+    go client.ReadFromUser(conn, reader, messages)
+    
+    // Create a new channel for receiving messages from the server
+    go client.ReadFromServer(conn, messages)
 
     // Continuously check for messages from the server
     for {
         select {
-        case msg := <-messages:
+        case msg := <- messages:
             fmt.Println(msg)
         }
     }
---
+++ pkg/client/client.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func ReadFromServer(conn net.Conn, messages chan string) {
+	buf := make([]byte, 64)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		text := string(buf[0:n])
+		messages <- text
+	}
+}
+
+func ReadFromUser(conn net.Conn, reader *bufio.Reader, messages chan string) {
+	// read from user input
+	input, err := reader.ReadString('\n') // newline is delimeter to read until
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn.Write([]byte(input))
+}
