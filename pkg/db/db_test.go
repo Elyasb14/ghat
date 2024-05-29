ff028eab8d3daa53cc5546c1ea0bbf2b8310a197
Elyas Bianchi <elyasb99@gmail.com>
2024-05-29 09:58:33 -0700

    blah

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -9,9 +9,4 @@ import (
 func TestDb(t *testing.T) {
 	Db := db.InitDB("file:/tmp/db.go")
 
-	Db.WriteMSG("hello", "192.168.33.1")
-	Db.WriteMSG("fuck", "192.168.33.1")
-	Db.WriteMSG("nuts", "10.10.10.1")
-
-	t.Log(Db.ReadDB())
 }
